850e6511a5826a7c4ab9441e8a0a0889f5dbc229
Jaremy Creechley <jaremy.creechley@panthalassa.com>
2021-11-09 15:04:21 -0800

    moving over basic utils used between projects

--- src/mcu_utils/logging.nim
+++ src/mcu_utils/logging.nim
@@ -15,19 +15,19 @@ const
 template initLogging*(args: varargs[untyped]) = 
   discard args
 
-macro logImpl(level: static[Level]; args: varargs[string, `$`]) =
+macro logImpl(level: static[Level]; msg: string, args: varargs[string, `$`]) =
   let lvl: int = level.ord()
   if lvl >= ord(mcuUtilsLevel):
     # result = nnkStmtList.newTree()
     # let v = args.mapIt(newCall("$", it))
-    result.add newCall("echo", args)
+    result.add newCall("echo", msg, args)
 
-template log*(level: static[Level], args: varargs[string, `$`]) = logImpl(lvlDebug, args) 
+template log*(level: static[Level], msg: string, args: varargs[string, `$`]) = logImpl(lvlDebug, msg, args) 
 
-template logDebug*(args: varargs[string, `$`]) = logImpl(lvlDebug, args) 
-template logError*(args: varargs[string, `$`]) = logImpl(lvlError, args) 
-template logFatal*(args: varargs[string, `$`]) = logImpl(lvlFatal, args) 
-template logInfo*(args: varargs[string, `$`]) = logImpl(lvlInfo, args) 
-template logWarn*(args: varargs[string, `$`]) = logImpl(lvlWarn, args) 
-template logNotice*(args: varargs[string, `$`]) = logImpl(lvlNotice, args) 
+template logDebug*(msg: string, args: varargs[string, `$`]) = logImpl(lvlDebug, msg, args) 
+template logError*(msg: string, args: varargs[string, `$`]) = logImpl(lvlError, msg, args) 
+template logFatal*(msg: string, args: varargs[string, `$`]) = logImpl(lvlFatal, msg, args) 
+template logInfo*(msg: string, args: varargs[string, `$`]) = logImpl(lvlInfo, msg, args) 
+template logWarn*(msg: string, args: varargs[string, `$`]) = logImpl(lvlWarn, msg, args) 
+template logNotice*(msg: string, args: varargs[string, `$`]) = logImpl(lvlNotice, msg, args) 
 
