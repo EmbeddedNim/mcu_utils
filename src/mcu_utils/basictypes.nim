d7a393fd12fac278b9709238f34f4428202b11d2
Jaremy Creechley <jaremy.creechley@panthalassa.com>
2021-11-12 19:26:12 -0800

    misc fixes

--- src/mcu_utils/basictypes.nim
+++ src/mcu_utils/basictypes.nim
@@ -5,3 +5,12 @@ type
 
   Millis* = distinct int64
   Micros* = distinct int64
+
+template basicMathBorrows(T: untyped) =
+  proc `+` *(x, y: T): T {.borrow.}
+  proc `-` *(x, y: T): T {.borrow.}
+  proc `<` *(x, y: T): bool {.borrow.}
+  proc `<=` *(x, y: T): bool {.borrow.}
+
+basicMathBorrows(Millis)
+basicMathBorrows(Micros)
