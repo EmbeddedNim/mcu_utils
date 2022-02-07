f198942a5a719a00a7b5ad1e8389a6d843920d6d
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-02-06 21:10:40 -0800

    tweak event fd

--- src/mcu_utils/eventfds.nim
+++ src/mcu_utils/eventfds.nim
@@ -1,6 +1,17 @@
+import std/nativesockets
+import std/os
+
+export SocketHandle
 
 type
-  EventFd = uint64
+  EventCount = uint64
 
-proc eventfd*(count: cuint, flags: cint): cint
+proc eventfd(count: cuint, flags: cint): cint
      {.cdecl, importc: "eventfd", header: "<sys/eventfd.h>".}
+
+proc newEventFd*(init: EventCount, flags: int): SocketHandle =
+  let res = eventfd(init.cuint, flags.cint)
+  if res < 0:
+    raiseOSError(osLastError())
+  
+  result = SocketHandle(res)
