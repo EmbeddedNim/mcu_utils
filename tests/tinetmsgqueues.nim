280a9a97e9e6fdc912cc7818baedf45f20629d87
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-02-09 20:25:04 -0800

    adding inet msg queue

---
+++ tests/tinetmsgqueues.nim
@@ -0,0 +1,64 @@
+import std/os
+import std/random
+
+import mcu_utils/inetqueues
+
+proc producerThread(args: (InetMsgQueue, int, int)) =
+  var
+    myFifo = args[0]
+    count = args[1]
+    tsrand = args[2]
+  echo "\n===== running producer ===== "
+  for i in 0..<count:
+    os.sleep(rand(tsrand))
+    # /* create data item to send */
+    var txData = newQMsgBuffer("txNum" & $(1234 + 100 * i))
+
+    # /* send data to consumers */
+    echo "-> Producer: tx_data: putting: ", i, " -> ", repr(txData)
+    myFifo.sendMsg(InetClientHandle.empty(), txData)
+    echo "-> Producer: tx_data: sent: ", i
+  echo "Done Producer: "
+  
+proc consumerThread(args: (InetMsgQueue, int, int)) =
+  var
+    myFifo = args[0]
+    count = args[1]
+    tsrand = args[2]
+  echo "\n===== running consumer ===== "
+  for i in 0..<count:
+    os.sleep(rand(tsrand))
+    echo "<- Consumer: rx_data: wait: ", i
+    var item: InetMsgQueueItem = myFifo.recvMsg()
+    let rxData = move item.data
+    echo "<- Consumer: rx_data: got: ", i, " <- ", repr(rxData)
+
+  echo "Done Consumer "
+
+proc runTestsChannel*() =
+  randomize()
+  var myFifo = InetMsgQueue.init(10)
+
+
+  producerThread((myFifo, 10, 100))
+  consumerThread((myFifo, 10, 100))
+
+proc runTestsChannelThreaded*(ncnt, tsrand: int) =
+  echo "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+  echo "[Channel] Begin "
+  randomize()
+  var myFifo = InetMsgQueue.init(10)
+
+  var thrp: Thread[(InetMsgQueue, int, int)]
+  var thrc: Thread[(InetMsgQueue, int, int)]
+
+  createThread(thrc, consumerThread, (myFifo, ncnt, tsrand))
+  # os.sleep(2000)
+  createThread(thrp, producerThread, (myFifo, ncnt, tsrand))
+  joinThreads(thrp, thrc)
+  echo "[Channel] Done joined "
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+
+when isMainModule:
+  runTestsChannelThreaded(100, 120)
+  runTestsChannelThreaded(7, 1200)
