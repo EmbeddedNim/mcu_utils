b7c03817b813168fe106f3adeba421ea651eb44b
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-03-07 19:59:43 -0800

    rename to timeutils

--- src/mcu_utils/timeutils.nim
+++ src/mcu_utils/timeutils.nim
@@ -7,6 +7,10 @@ when defined(zephyr):
 elif defined(arduino):
   proc arduinoMillis(): culong {.importc: "$1", header: "<Arduino.h>".}
   proc millis*(): Millis = Millis(cast[int64](arduinoMillis()))
+elif defined(linux):
+  import std/monotimes
+  proc millis*(): Millis = 
+    let ts = getMonoTime()
 else:
   proc millis*(): Millis = 
     raise newException(OSError, "millis unimplemented")
