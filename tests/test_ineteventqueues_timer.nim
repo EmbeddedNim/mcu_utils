16cc2256ffa8b33e1cbab2f67a8f35f7c4b01dc5
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-05-31 23:03:02 -0700

    adding timer events

--- tests/test_ineteventqueues_timer.nim
+++ tests/test_ineteventqueues_timer.nim
@@ -12,8 +12,10 @@ type
     count: int
     tsrand: int
 
-proc timerThread(args: ThreadArgs) {.thread.} =
-  discard
+
+## Produce Queue Events
+## 
+
 
 proc produceQueueEvents(args: ThreadArgs) {.thread.} =
   echo "\n===== running producer ===== "
@@ -63,56 +65,92 @@ proc consumeQueueEvents(args: ThreadArgs) {.thread.} =
     
   echo "Done Consumer "
 
-proc consumerThread(args: ThreadArgs) {.thread.} =
-  var queue = args.queue
+## Produce Queue Events
+## 
 
+proc produceTimeEvents(args: ThreadArgs) {.thread.} =
+  echo "\n===== running producer ===== "
+  for i in 1 .. args.count:
+    os.sleep(rand(args.tsrand))
+    # /* create data item to send */
+    var txData = "txNum" & $(1234 + 100 * i)
+
+    # /* send data to consumers */
+    echo "-> Producer: tx_data: putting: ", i, " -> ", repr(txData)
+    args.queue.send(txData, trigger=false)
+
+    if i mod 4 == 0:
+      args.queue.trigger()
+    echo "-> Producer: tx_data: sent: ", i
+
+  args.queue.trigger()
+  echo "Done Producer: "
+  
+proc consumeTimeEvents(args: ThreadArgs) {.thread.} =
+  ## consumer using manual queue events
+  ## 
+  ## for example if you want to batch multiple items into a queue
+  ## then process them in batches
+  ## 
   echo "\n===== running consumer ===== "
+  var queue = args.queue
   var selector = newEventSelector()
-  selector.registerQueue(queue)
-  let defaultTimer = selector.registerTimer(timeout=10, oneshot=true)
-  echo fmt"created timer: {defaultTimer=}"
+  let queueHasDataEvent = selector.registerQueue(queue)
+  echo fmt"{queueHasDataEvent=}"
 
   var events: Table[InetEvent, ReadyKey]
+  var count = 0
 
   loop(selector, -1.Millis, events):
-    # check specific event
-    if defaultTimer in events:
-      echo fmt"<- Consumer: default timer triggered! "
+    echo fmt"<- Consumer: has data event? {queueHasDataEvent in events =} "
+    withEvent(events, queueHasDataEvent, asKey=readyKey):
+      echo fmt"<- Consumer: got data event: {queueHasDataEvent=} with {readyKey=}"
+
+      var rxData: string
+      while queue.tryRecv(rxData):
+        inc count
+        echo "<- Consumer: rx_data: got: ", count, " <- ", repr(rxData)
     
-    # print all events
-    for evt, key in events:
-      echo fmt"<- Consumer: event occurred: {evt=} with {key=}"
-    
+    if count >= args.count:
+      break
     
   echo "Done Consumer "
 
 proc runTestsChannelThreaded*(ncnt, tsrand: int) =
-  echo "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
-  echo "[Channel] Begin "
-  randomize()
-  var myFifo = InetEventQueue[string].init(4)
-
-  var thrp: Thread[ThreadArgs]
-  var thrc: Thread[ThreadArgs]
-
-  createThread(thrc, consumeQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
-  # os.sleep(2000)
-  createThread(thrp, produceQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
-  joinThreads(thrp, thrc)
-  echo "[Channel] Done joined "
-  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
 
 suite "test for InetQueues functionality":
   echo "suite setup: run once before the tests"
   
+  setup:
+    randomize()
+
   test "timer":
     var cities = @["Cleveland", "Portland"]
     let cityStrings = cities.map do (x:string) -> string:
       "City of " & x
     echo fmt"{cityStrings=}"
 
-  test "faster threaded consumer/producer test":
-    runTestsChannelThreaded(11, 100)
+  test "queue event testing":
+    echo "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+    echo "[Channel] Begin "
+    var myFifo = InetEventQueue[string].init(4)
+    var thrp, thrc: Thread[ThreadArgs]
+    createThread(thrc, consumeQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
+    createThread(thrp, produceQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
+    joinThreads(thrp, thrc)
+    echo "[Channel] Done joined "
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+
+  test "timer event testing":
+    echo "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+    echo "[Channel] Begin "
+    var myFifo = InetEventQueue[string].init(4)
+    var thrp, thrc: Thread[ThreadArgs]
+    createThread(thrc, consumeQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
+    createThread(thrp, produceQueueEvents, ThreadArgs(queue: myFifo, count: ncnt, tsrand: tsrand))
+    joinThreads(thrp, thrc)
+    echo "[Channel] Done joined "
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
 
   # test "slow threaded consumer/producer test":
     # runTestsChannelThreaded(7, 1200)
